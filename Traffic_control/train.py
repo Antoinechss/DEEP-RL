92f4ea0cb307a0d0d753b1c42007bca7e3f84782
Antoine Chosson <antoine.chosson@eleves.enpc.fr>
2025-04-26 14:43:40 +0200

    Update train.py

--- Traffic_control/train.py
+++ Traffic_control/train.py
@@ -1,6 +1,10 @@
 from sumo_env import SumoIntersectionEnv
 from reinforce_agent import ReinforceAgent
 
+# Import the intersection sumo.cfg file and convert it into a gym environment
 env = SumoIntersectionEnv("/Users/antoinechosson/Desktop/intersection/1tls_2x2.sumocfg",use_gui=False)
+
+# Link and train reinforce agent in env
 agent = ReinforceAgent(env, gamma=0.99, lr=1e-4)
-agent.train(num_episodes=500)
+agent.policy.apply(agent._init_weights)
+agent.train(num_episodes=2000)
