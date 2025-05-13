dd0c61bb7b04af1dd8b39d9a755a239d62f9f35e
Antoine Chosson <antoine.chosson@eleves.enpc.fr>
2025-05-13 09:22:55 +0200

    Update train.py

--- Traffic_control/train.py
+++ Traffic_control/train.py
@@ -5,6 +5,6 @@ from reinforce_agent import ReinforceAgent
 env = SumoIntersectionEnv("/Users/antoinechosson/Desktop/intersection/1tls_2x2.sumocfg",use_gui=False)
 
 # Link and train reinforce agent in env
-agent = ReinforceAgent(env, gamma=0.99, lr=1e-4)
+agent = ReinforceAgent(env, gamma=0.99, lr=0.001)
 agent.policy.apply(agent._init_weights)
 agent.train(num_episodes=2000)
